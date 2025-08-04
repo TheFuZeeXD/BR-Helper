229658a33a0abf5ad1d0429e1d20a9396f43617f
fuzeexd <fuzeexd@thefuzeexd.ru>
2025-08-05 02:06:10 +0300

    Update popup.js

--- popup/popup.js
+++ popup/popup.js
@@ -20,7 +20,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
     async function getVersion() {
         try {
-            const rawUrl = 'https://raw.githubusercontent.com/TheFuZeeXD/BR-Helper/chrome/manifest.json';
+            const rawUrl = 'https://raw.githubusercontent.com/TheFuZeeXD/BR-Helper/refs/heads/chrome/manifest.json';
             const response = await fetch(rawUrl);
             const manifest = await response.json();
 
