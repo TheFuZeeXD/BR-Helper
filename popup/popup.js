9d431bbb6da1ffc84b8b7e5f8a2f9694d654ea26
TheFuZeeXD <fuzeexd@thefuzeexd.ru>
2025-08-05 01:33:05 +0300

    Update for FireFox

---
+++ popup/popup.js
@@ -0,0 +1,268 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const toggle = document.getElementById('extensionToggle');
+    const backgroundURL = document.querySelector("#backgroundURL-Toggle");
+    const backgroundURLtext = document.querySelector("#backgroundURL");
+    const themer = document.querySelector("#Theme-color");
+
+    // check avaiable version
+    let versionString = null;
+    async function main() {
+        versionString = await getVersion();
+        const myVersion = "1.4.1";
+        const warning = document.querySelector(".newUpdate");
+        if (myVersion != versionString) {
+            warning.style.opacity = "1";
+        } else {
+            warning.style.opacity = "0";
+        }
+    }
+    main();
+
+    async function getVersion() {
+        try {
+            const rawUrl = 'https://raw.githubusercontent.com/TheFuZeeXD/BR-Helper/FireFox/manifest.json';
+            const response = await fetch(rawUrl);
+            const manifest = await response.json();
+
+            return `${manifest.version}`;
+
+        } catch (error) {
+            console.error('Error fetching version:', error);
+            return null;
+        }
+    }
+
+
+
+    // toggle
+    if (localStorage.getItem("BackgroundURLbutton") == "true") {
+        backgroundURLtext.classList.add("backgroundURL-Checked");
+    }
+
+    browser.storage.sync.get(['enabled'], function (result) {
+        toggle.checked = result.enabled !== false;
+    });
+
+
+    toggle.addEventListener('change', function () {
+        browser.storage.sync.set({ enabled: this.checked });
+    });
+
+    // backgroundURL backgroundURLtext
+    browser.storage.sync.get(['disabled'], function (result) {
+        backgroundURL.checked = result.disabled !== false;
+    });
+
+
+    backgroundURL.addEventListener('change', function () {
+        browser.storage.sync.set({ disabled: this.checked });
+    });
+
+    // themer
+    browser.storage.sync.get(['themer'], function (result) {
+        themer.checked = result.themer !== false;
+    });
+
+
+    themer.addEventListener('change', function () {
+        browser.storage.sync.set({ themer: this.checked });
+    });
+
+    if (localStorage.getItem("Themer") == "true") {
+        document.body.classList.remove("light");
+    } else {
+        document.body.classList.add("light");
+    }
+
+});
+
+function saveCheckboxStates() {
+    const checkboxes = document.querySelectorAll('.multi-select input[type="checkbox"]');
+    checkboxes.forEach(checkbox => {
+        localStorage.setItem(`checkbox_${checkbox.value}`, checkbox.checked);
+    });
+}
+
+
+function restoreCheckboxStates() {
+    const checkboxes = document.querySelectorAll('.multi-select input[type="checkbox"]');
+    checkboxes.forEach(checkbox => {
+        const savedState = localStorage.getItem(`checkbox_${checkbox.value}`);
+        if (savedState !== null) {
+            checkbox.checked = savedState === 'true';
+        }
+    });
+}
+
+function updateServersList() {
+    const serverValues = Array.from(
+        document.querySelectorAll('.multi-select input[type="checkbox"]:not(:checked)')
+    ).map(checkbox => checkbox.value);
+
+    browser.storage.local.set({ ServersList: serverValues });
+}
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    restoreCheckboxStates();
+    updateServersList();
+});
+
+
+const checkboxes = document.querySelectorAll('.multi-select input[type="checkbox"]');
+checkboxes.forEach(checkbox => {
+    checkbox.addEventListener('change', function () {
+
+        localStorage.setItem(`checkbox_${this.value}`, this.checked);
+
+        updateServersList();
+
+        saveCheckboxStates();
+    });
+});
+
+
+const username = document.querySelector('#NickName');
+const rank = document.querySelector("#Rank");
+const myrules = document.querySelector("#rules");
+const check = document.querySelector("#extensionToggle");
+const backgroundURL = document.querySelector("#backgroundURL-Toggle");
+const MainBody = document.querySelector("#TitleText");
+const backgroundURLtext = document.querySelector("#backgroundURL");
+const themer = document.querySelector("#Theme-color");
+
+const managment = document.getElementById("popup-header-button_managment");
+const settings = document.getElementById("popup-header-button_settings");
+const customization = document.getElementById("popup-header-button_customization");
+const credits = document.getElementById("popup-header-button_credits");
+
+document.getElementById("popup-header-button_managment").addEventListener('click', () => {
+    managment.classList.add("activePage");
+    settings.classList.remove("activePage");
+    customization.classList.remove("activePage");
+    credits.classList.remove("activePage");
+    document.querySelector("#Screen-Chrome-Exst_house").style.display = "block";
+    document.querySelector("#Screen-Chrome-Exst_settings").style.display = "none";
+    document.querySelector("#Screen-Chrome-Exst_customization").style.display = "none";
+    document.querySelector("#Screen-Chrome-Exst_credits").style.display = "none";
+});
+
+document.getElementById("popup-header-button_settings").addEventListener('click', () => {
+    settings.classList.add("activePage");
+    managment.classList.remove("activePage");
+    customization.classList.remove("activePage");
+    credits.classList.remove("activePage");
+    document.querySelector("#Screen-Chrome-Exst_house").style.display = "none";
+    document.querySelector("#Screen-Chrome-Exst_settings").style.display = "block";
+    document.querySelector("#Screen-Chrome-Exst_customization").style.display = "none";
+    document.querySelector("#Screen-Chrome-Exst_credits").style.display = "none";
+});
+
+document.getElementById('popup-header-button_customization').addEventListener('click', () => {
+    settings.classList.remove("activePage");
+    managment.classList.remove("activePage");
+    customization.classList.add("activePage");
+    credits.classList.remove("activePage");
+    document.querySelector("#Screen-Chrome-Exst_house").style.display = "none";
+    document.querySelector("#Screen-Chrome-Exst_settings").style.display = "none";
+    document.querySelector("#Screen-Chrome-Exst_customization").style.display = "block";
+    document.querySelector("#Screen-Chrome-Exst_credits").style.display = "none";
+});
+
+document.getElementById('popup-header-button_credits').addEventListener('click', () => {
+    settings.classList.remove("activePage");
+    managment.classList.remove("activePage");
+    customization.classList.remove("activePage");
+    credits.classList.add("activePage");
+    document.querySelector("#Screen-Chrome-Exst_house").style.display = "none";
+    document.querySelector("#Screen-Chrome-Exst_settings").style.display = "none";
+    document.querySelector("#Screen-Chrome-Exst_customization").style.display = "none";
+    document.querySelector("#Screen-Chrome-Exst_credits").style.display = "block";
+});
+
+
+if (localStorage.getItem("Rules") == undefined) {
+    localStorage.setItem("Rules", "admin");
+    browser.storage.local.set({ Rules: "admin" });
+};
+
+if (localStorage.getItem("Active") == undefined) {
+    localStorage.setItem("Active", true);
+    browser.storage.local.set({ Active: true });
+};
+
+username.addEventListener("input", function () {
+    localStorage.setItem("NickName", this.value);
+    browser.storage.local.set({ NickName: this.value });
+});
+
+rank.addEventListener("input", function () {
+    localStorage.setItem("Rank", this.value);
+    browser.storage.local.set({ Rank: this.value });
+});
+
+backgroundURLtext.addEventListener("input", function () {
+    const ImagePath = this.value;
+    localStorage.setItem("backgroundURL", ImagePath);
+    browser.storage.local.set({ backgroundURL: ImagePath });
+});
+
+window.addEventListener("load", function () {
+    const getNickName = localStorage.getItem("NickName");
+    const getRank = localStorage.getItem("Rank");
+    const getBackgrountext = localStorage.getItem("backgroundURL");
+
+    if (getBackgrountext) backgroundURLtext.value = getBackgrountext;
+    if (getNickName) username.value = getNickName;
+    if (getRank) rank.value = getRank;
+});
+
+
+check.addEventListener("change", function () {
+    if (this.checked) {
+        localStorage.setItem("Active", true);
+        browser.storage.local.set({ Active: true });
+    } else {
+        localStorage.setItem("Active", false);
+        browser.storage.local.set({ Active: false });
+    }
+});
+
+
+
+themer.addEventListener("change", function () {
+    if (this.checked) {
+        localStorage.setItem("Themer", true);
+        document.body.classList.remove("light");
+    } else {
+        localStorage.setItem("Themer", false);
+        document.body.classList.add("light");
+    }
+});
+
+
+
+backgroundURL.addEventListener("change", function () {
+    const inputURL = document.querySelector("#backgroundURL");
+    if (this.checked) {
+        inputURL.classList.add("backgroundURL-Checked");
+        localStorage.setItem("BackgroundURLbutton", true);
+        browser.storage.local.set({ BackgroundURLbutton: true });
+    } else {
+        inputURL.classList.remove("backgroundURL-Checked");
+        localStorage.setItem("BackgroundURLbutton", false);
+        browser.storage.local.set({ BackgroundURLbutton: false });
+    }
+});
+
+
+
+myrules.addEventListener("change", function () {
+    browser.storage.local.set({ Rules: this.value });
+    localStorage.setItem("Rules", this.value);
+})
+
+window.addEventListener("load", function () {
+    const getRules = localStorage.getItem("Rules");
+    if (getRules) myrules.value = getRules;
+});
