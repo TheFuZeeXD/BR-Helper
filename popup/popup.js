9aa5613a7a242ce658b26b563a096885f1b873ed
TheFuZeeXD <fuzeexd@thefuzeexd.ru>
2025-08-05 13:18:28 +0300

    Add GitHub API

--- popup/popup.js
+++ popup/popup.js
@@ -4,33 +4,41 @@ document.addEventListener('DOMContentLoaded', function () {
     const backgroundURLtext = document.querySelector("#backgroundURL");
     const themer = document.querySelector("#Theme-color");
 
-    // check avaiable version
+    // Check version by GitHub API
     let versionString = null;
+
     async function main() {
         versionString = await getVersion();
+        console.log(versionString);
         const myVersion = "1.4.1";
         const warning = document.querySelector(".newUpdate");
-        if (myVersion != versionString) {
-            warning.style.opacity = "1";
-        } else {
-            warning.style.opacity = "0";
+
+        if (warning) {
+            warning.style.opacity = (myVersion != versionString) ? "1" : "0";
         }
     }
-    main();
 
     async function getVersion() {
         try {
-            const rawUrl = 'https://raw.githubusercontent.com/TheFuZeeXD/BR-Helper/refs/heads/chrome/manifest.json';
-            const response = await fetch(rawUrl);
-            const manifest = await response.json();
+            const apiUrl = 'https://api.github.com/repos/TheFuZeeXD/BR-Helper/contents/manifest.json?ref=chrome';
+            const response = await fetch(apiUrl);
+            const data = await response.json();
 
-            return `${manifest.version}`;
+
+            const decodedContent = atob(data.content.replace(/\s/g, ''));
+            const manifest = JSON.parse(decodedContent);
+
+            return manifest.version;
 
         } catch (error) {
             console.error('Error fetching version:', error);
             return null;
         }
     }
+
+
+    main();
+
 
 
 
