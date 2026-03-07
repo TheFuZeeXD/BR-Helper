21f4bd219980e479a8fd9cac98e0da0ab3ce5ad2
TheFuZeeXD <contact@thefuzeexd.com>
2026-03-07 15:33:51 +0300

    Update version 1.4.5-alpha.0

--- popup/functions/version-update.js
+++ popup/functions/version-update.js
@@ -1,7 +1,7 @@
     let versionString = null;
     async function main() {
         versionString = await getVersion();
-        const myVersion = "1.4.4";
+        const myVersion = "1.4.5-alpha.0";
         const warning = document.querySelector(".newUpdate");
 
         if (warning) {
